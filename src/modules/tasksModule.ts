9b989145a7b25c371450a0367d1c8ff16b55554e
Bo <omuqqz@gmail.com>
2023-06-22 23:42:40 +0300

    update task module

--- src/modules/tasksModule.ts
+++ src/modules/tasksModule.ts
@@ -2,16 +2,19 @@ import { BotContext } from "interfaces.js"
 import { getUserByTelegramId } from "./database/database.js"
 import { Composer, Scenes, session } from "telegraf";
 import { introduceScene, introduceComposer, introduceSceneName } from "../scenes/introduce.js";
+import { tasksSceneName } from "../scenes/tasks.js";
 
 
 export const tasksComposer = new Composer();
 
-export const taskService = async (ctx: BotContext) => {
+export const taskService = async (ctx: BotContext) => { 
     const user = await getUserByTelegramId(ctx.from.id);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ctx.session.user = user;
     if (!user) {
         ctx.scene.enter(introduceSceneName);
     } else {
-        ctx.reply('Hello, ' + user.name);
-        console.log(user);
+        ctx.scene.enter(tasksSceneName);
     }
 }
