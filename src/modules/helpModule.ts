35a6a1e912ba117621fcc69938c697d3c13c8dc4
Bo <omuqqz@gmail.com>
2023-06-22 23:42:22 +0300

    update help module

--- src/modules/helpModule.ts
+++ src/modules/helpModule.ts
@@ -3,10 +3,11 @@ import { Context } from "telegraf";
 export const sendDescription = async (ctx: Context) => {
     try {
         ctx.replyWithHTML([
-            'What can i propose for you:',
+            'These are my services:',
             '/cat - Random cat photo',
             '/dog - Random dog photo',
             '/weather - Weather service',
+            '/tasks - Tasks service',
         ].join('\n'));
 
     } catch (e) {
