f2b2de38c982a610d69128c7e2c7a12e794e3dcb
Bo <omuqqz@gmail.com>
2023-06-24 22:11:40 +0300

    add helper convertToTime

---
+++ src/helpers/convertToTime.ts
@@ -0,0 +1,9 @@
+import { Time } from "../interfaces.js";
+
+export const convertToTime = (time: string): Time | null => {
+    const digits = time.match(/[0-2]?[0-9]?.*[0-6]?[0-9]?/)[0]?.match(/[0-9]{1,2}/g);
+    if (!digits || !digits.length || digits.length !== 2) return null;
+    const [h, m] = digits;
+    if (+h < 24 && +m < 60) return {hours: +h, minutes: +m};
+    return null;
+}
