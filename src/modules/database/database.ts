b1aa5399de81e29ac23bf50c5fa1a822aa1420ed
Bo <omuqqz@gmail.com>
2023-06-29 18:42:32 +0300

    update database api

--- src/modules/database/database.ts
+++ src/modules/database/database.ts
@@ -1,26 +1,16 @@
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv';
-import { Task, User, Subscription } from "./models.js";
-import { DBSubscription, DBTask, DBUser, Time } from "../../interfaces.js";
+import { Task, User, Subscription } from "./models/index.js";
+import { DBSubscription, DBTask, DBUser, Time } from "@/interfaces/interfaces.js";
+import { dbUrl } from "@/config.js";
+import { tryCatchWrapper } from "@/helpers/tryCatchWrapper.js";
 
-
-dotenv.config()
-
-const url = process.env.DB_URL as string;
-
-mongoose.connect(url);
+mongoose.connect(dbUrl);
 
 export async function getUserByTelegramId(id: number) {
-    try {
-        const user = await User.findOne<DBUser>({telegramId: id});
-        return user;
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
+    return await tryCatchWrapper<DBUser>(async () => await User.findOne<DBUser>({ telegramId: id }))
 }
 
-export async function createUser({telegramId, name, city}:{telegramId: number, name: string, city: string | undefined}) {
+export async function createUser({ telegramId, name, city }: { telegramId: number, name: string, city: string | undefined }) {
     try {
         await User.create({
             name,
@@ -32,14 +22,14 @@ export async function createUser({telegramId, name, city}:{telegramId: number, n
     }
 }
 
-export async function getTasksByUserId (id: mongoose.Types.ObjectId){
+export async function getTasksByUserId(id: mongoose.Types.ObjectId) {
     const tasks = await Task.find<DBTask>({
         userId: id,
     })
     return tasks;
 }
 
-export async function createTask ({userId, date, description}: {userId: mongoose.Types.ObjectId, date: Date, description: string}) {
+export async function createTask({ userId, date, description }: { userId: mongoose.Types.ObjectId, date: Date, description: string }) {
     Task.create({
         userId,
         date,
@@ -47,54 +37,42 @@ export async function createTask ({userId, date, description}: {userId: mongoose
     })
 }
 
-export async function deleteTask (taskId: string) {
+export async function deleteTask(taskId: string) {
     try {
-        const res = await Task.deleteOne({
+        await Task.deleteOne({
             _id: taskId,
         });
-        console.log(res)
     } catch (e) {
         console.log(e);
     }
 }
 
-export async function getSubscription (userId: mongoose.Types.ObjectId) {
-    try {
-        const subscription = await Subscription.findOne<DBSubscription>({
+export async function getSubscription(userId: mongoose.Types.ObjectId) {
+    return await tryCatchWrapper(async () => {
+        await Subscription.findOne<DBSubscription>({
             userId,
         })
-        console.log(subscription);
-        return subscription;
-    } catch(e) {
-        return null;
-    }
+    })
 }
 
-export async function createSubscription (userId: mongoose.Types.ObjectId, time:Time, city: string, chatId: number) {
-    try {
-        const sub = await Subscription.create<DBSubscription>({
+export async function createSubscription(userId: mongoose.Types.ObjectId, time: Time, city: string, chatId: number) {
+    return await tryCatchWrapper(async () => {
+        return await Subscription.create<DBSubscription>({
             userId,
             time,
             city,
             chatId,
         })
-        return sub;
-    } catch (e) {
-        return null;
-    }
+    })
 }
 
-export async function deleteSubscription (subscriptionId: mongoose.Types.ObjectId) {
-    try {
-        await Subscription.deleteOne({
-            _id: subscriptionId.toString(),
-        })
-    } catch(e) {
-        return null
-    }
+export async function deleteSubscription(subscriptionId: mongoose.Types.ObjectId) {
+    await Subscription.deleteOne({
+        _id: subscriptionId.toString(),
+    })
 }
 
-export async function getAllSubscribtions () {
+export async function getAllSubscribtions() {
     try {
         const subs = await Subscription.find<DBSubscription>({}).populate<DBUser>('userId');
         return subs;
