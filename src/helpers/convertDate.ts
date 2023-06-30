beca30a1f292107afb7007406cbc0e07dd8d92d1
Bo <omuqqz@gmail.com>
2023-06-30 13:00:04 +0300

    correct mistake in convertDate

--- src/helpers/convertDate.ts
+++ src/helpers/convertDate.ts
@@ -1,11 +1,11 @@
 export const convertDate = (str: string): Date | null => {
     // Following regExp describe date template in format DD:MM:YYYY HH:MM
-    // Example: 21.12.2012 0.0     14/6/2023 23/59
-    const matches = str.match(/[0-3]?[0-9].[0-1]?[0-9].202[3-9].{1}[0-2]?[0-9].[0-6]?[0-9]/);
+    // Example: 21.12.2026 0.0 | 14/6/2023 23/59
+    const matches = str.match(/[0-3]?[0-9].[0-1]?[0-9].202[3-9].[0-2]?[0-9].[0-5]?[0-9]/);
     if (!matches || !matches.length) return null;
     const date = matches[0];
     // Following regExp describe any number that has 1-4 digits 
-    // Example: 21.12.2012 0.0     14/6/2023 23/59
+    // Example: 1 | 23 | 2024
     const [d, m, y, h, min] = date.match(/[0-9]{1,4}/gi).slice(0, 5);
     if (!isValidDate(+d, +m - 1, +y) || !isValidTime(+h, +min)) return null;
     const convertedDate = new Date(+y, +m - 1, +d, +h, +min);
